a1984dca99794a6e5ee8d5cbb0bd37e79d9b9492
uc4151 <gemmalinda19@gmail.com>
2023-11-17 08:22:23 -0800

    second redo

--- handle_print.c
+++ handle_print.c
@@ -2,12 +2,12 @@
 
 /**
  * handle_print - Prints an argument based on its type
- * @fmt: Formatted string for printing the arguments.
+ * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
- * @width: specifies width
+ * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
@@ -17,7 +17,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'c', printed_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
